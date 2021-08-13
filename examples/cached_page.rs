bae1d82a82953ffbd2d86a1f2b09e2b0c8097db6
Magic Len <len@magiclen.org>
2021-08-13 15:13:29 +0800

    use `rocket = "0.5.0-rc.1"`

--- examples/cached_page.rs
+++ examples/cached_page.rs
@@ -11,17 +11,34 @@ use std::io::Cursor;
 use rocket_etag_if_none_match::{entity_tag::EntityTag, EtagIfNoneMatch};
 
 use rocket::http::Status;
-use rocket::response::{Response, Result};
+use rocket::request::Request;
+use rocket::response::{Responder, Response, Result};
 
 use chrono::prelude::*;
 
 static MY_ETAG: EntityTag = unsafe { EntityTag::new_unchecked(true, Cow::Borrowed("MAGIC")) };
 
+struct MyResponse<'r>(Response<'r>);
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for MyResponse<'o> {
+    #[inline]
+    fn respond_to(self, _: &'r Request<'_>) -> Result<'o> {
+        Ok(self.0)
+    }
+}
+
+impl<'r> From<Response<'r>> for MyResponse<'r> {
+    #[inline]
+    fn from(res: Response<'r>) -> Self {
+        MyResponse(res)
+    }
+}
+
 #[get("/")]
-fn index(etag_if_none_match: EtagIfNoneMatch) -> Result<'static> {
+fn index(etag_if_none_match: EtagIfNoneMatch) -> MyResponse<'static> {
     if etag_if_none_match.weak_eq(&MY_ETAG) {
         println!("Cached!");
-        Response::build().status(Status::NotModified).ok()
+        Response::build().status(Status::NotModified).finalize().into()
     } else {
         let body = format!("Current Time: {}\n\nTry to re-open this page repeatedly without pressing the forced-refresh(Ctrl+F5) button.", Utc::now().to_rfc3339());
 
@@ -31,7 +48,8 @@ fn index(etag_if_none_match: EtagIfNoneMatch) -> Result<'static> {
             .raw_header("Etag", MY_ETAG.to_string())
             .raw_header("Content-Type", "text/plain; charset=utf-8")
             .sized_body(size, Cursor::new(body))
-            .ok()
+            .finalize()
+            .into()
     }
 }
 
